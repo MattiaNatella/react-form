c0ae7882fa63ae6ac3f7625910e4953eb1bd0614
MattiaNatella <mattianatella@gmail.com>
2025-01-19 21:34:09 +0100

    First Commit plus base exercise completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Main } from "./components/Main"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+
+  return (
+    <>
+      <Main />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+export const Main = () => {
+
+    const [titles, setTitles] = useState([])
+    const [newTitle, setNewTitle] = useState('')
+
+    const handlerSubmit = (e) => {
+        e.preventDefault();
+        let blog = [newTitle, ...titles]
+        setTitles(blog)
+        setNewTitle('')
+    }
+
+    const deleteTitle = (index) => {
+        const filteredTitles = titles.filter(title => title !== titles[index])
+        setTitles(filteredTitles)
+
+    }
+
+    return (
+        <main className="container">
+            <form action="#" onSubmit={handlerSubmit}>
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Titolo Blog" aria-label="ciao" aria-describedby="button-addon2" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} />
+                    <button className="btn btn-outline-secondary" type="button submit" id="button-addon2">Invia</button>
+                </div>
+            </form>
+            {titles.length < 1 ? <h1>Nessun titolo inserito</h1> : (
+                <ul className="list-group">
+                    {titles.map((item, index) => (
+                        <li key={index} className="list-group-item d-flex justify-content-between align-items-center">{item} <i className="fa-solid fa-trash" onClick={() => deleteTitle(index)}></i></li>
+
+                    ))}
+                </ul>
+            )}
+
+
+
+        </main>
+    )
+}
